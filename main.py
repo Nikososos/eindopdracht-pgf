eb22c596821f63d95de179fffe16a4dfff9d9437
Nikososos <nickywolf@live.nl>
2025-07-03 12:02:16 +0200

    Uitschrijven van de functies in comments zodat ik weet wat ik wil coden

---
+++ main.py
@@ -0,0 +1,17 @@
+# App word opgebouwd in verschillende handige functies
+
+# functie 1 Maaltijd of ingredient opzoeken
+# Gebruiker typt in bijvoorbeeld kipfilet of banaan
+# Gebruiker krijgt voedingswaarden terug per 100g opgedeeld in KCAL, EIWITTEN, VETTEN EN KOOLHYDRATEN
+
+# functie 2 dagelijkse totaalwaarden berekenen
+# Voeg meerdere maaltijden toe en houd dagtotaal bij
+# Wederom opgedeeld in KCAL, EIWITTEN, VETTEN EN KOOLHYDRATEN
+
+# functie 3 Opslaan van dagelijkse voeding in een lokaal bestaand
+# bijhouden wat je vandaag en op eerdere dagen heb gegeten
+
+# functie 4 persoonlijke macrodoelen instellen
+# De app vergelijkt je huidige dagtotaal en berekend hoeveel je nog moet eten
+
+#Daarnaast moet er ook een command line menu komen om de functies aan te roepen en terug te keren naar het menu!
